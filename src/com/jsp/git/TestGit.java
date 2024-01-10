ad4d7b023abd5cb59456968f55bf5e33e8b65842
amar kumbar <137049443+Amarrk11@users.noreply.github.com>
2024-01-10 10:08:52 +0530

    changes undone

--- src/com/jsp/git/TestGit.java
+++ src/com/jsp/git/TestGit.java
@@ -4,17 +4,6 @@ public class TestGit {
 	public static void main(String[] args) {
 		System.out.println("amar");
 		System.out.println("shashi");
-		System.out.println("nagaraj");
-		System.out.println("amar");
-		System.out.println("shashi");
-		System.out.println("nagaraj");System.out.println("amar");
-		System.out.println("shashi");
-		System.out.println("nagaraj");System.out.println("amar");
-		System.out.println("shashi");
-		System.out.println("nagaraj");System.out.println("amar");
-		System.out.println("shashi");
-		System.out.println("nagaraj");System.out.println("amar");
-		System.out.println("shashi");
-		System.out.println("nagaraj");
+	
 	}
 }
