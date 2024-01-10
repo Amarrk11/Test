e3093c47cee689fd0dcf4527bf73d73f00d004ce
amar kumbar <137049443+Amarrk11@users.noreply.github.com>
2024-01-10 10:27:33 +0530

    changed

--- src/com/jsp/git/TestGit.java
+++ src/com/jsp/git/TestGit.java
@@ -2,9 +2,7 @@ package com.jsp.git;
 
 public class TestGit {
 	public static void main(String[] args) {
-		System.out.println("amar");
-		System.out.println("shashi");
-		System.out.println("shashi");
+		
 
 	
 	}
